0db451c1807c8da011d4ee63bc5b29f399918338
Matt Hastings <matthastings85@gmail.com>
2022-02-16 09:34:37 -0600

    styling tweaks

--- src/components/TurnTransition/index.js
+++ src/components/TurnTransition/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 // Components
+import Media from "react-media";
 import Scorecard from "../Scorecard";
 import Button from "../Button";
 
@@ -10,7 +11,7 @@ const TurnTransition = ({ callback, player }) => {
   };
   return (
     <div>
-      <Scorecard />
+      <Media query="(max-width: 1000px)" render={() => <Scorecard />} />
       <h3>Next Player: {player}</h3>
       <Button text="Start Round" callback={handleClick} id="next-btn" />
     </div>
